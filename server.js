b3bd5489efedbac17ff619c1aa49c762c4361a6f
Shweta Jadhav <shwetajadhav2324@gmail.com>
2025-12-03 10:24:22 +0530

    change index file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 let highScore = 0;
 
 app.use(bodyParser.json());
